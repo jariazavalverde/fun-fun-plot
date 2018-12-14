b2d9577373728b7f19bc510038831aceb04d4823
jariazavalverde <riazavalverde@gmail.com>
2018-12-14 01:10:31 +0100

    added operators and plots

--- src/primitives.py
+++ src/primitives.py
@@ -1,3 +1,21 @@
+class Plot:
+	"""This class stores a canvas for drawing."""
+	
+	def __init__(self, component, canvas, width, height):
+		self.component = component
+		self.canvas = canvas(width, height)
+		self.width = width
+		self.height = height
+		# (left, right, top, bottom)
+		self.dimensions = (float('inf'), float('-inf'), float('-inf'), float('inf'))
+	
+	def draw(self, data):
+		"""This method plots a dataset."""
+		length = len(data)
+		self.component.eval(self, data, length).draw(self)
+
+
+
 class Element:
 	"""This class defines the interface for any graphical component."""
 		
@@ -5,7 +23,7 @@ class Element:
 		"""This method returns all the parameters of the constructor."""
 		return []
 	
-	def compute(self, canvas):
+	def compute(self, plot):
 		"""This method computes some properties of the graphical component."""
 		pass
 	
@@ -13,10 +31,10 @@ class Element:
 		"""This method draws the graphical component in the canvas."""
 		pass
 	
-	def eval(self, canvas, data, elem):
+	def eval(self, plot, data, elem):
 		"""This method returns an evaluated instance of the graphical component."""
 		return self.__class__(
-			*list(map(lambda x: x.eval(canvas, data, elem),
+			*list(map(lambda x: x.eval(plot, data, elem),
 				self.get_attributes())))
 	
 	def __add__(self, elem):
@@ -35,10 +53,56 @@ class Compose(Element):
 	def get_attributes(self):
 		return [self.left, self.right]
 	
-	def compute(self, canvas):
-		self.left.compute(canvas)
-		self.right.compute(canvas)
+	def compute(self, plot):
+		self.left.compute(plot)
+		self.right.compute(plot)
 	
-	def draw(self, canvas):
-		self.left.draw(canvas)
-		self.right.draw(canvas)
+	def draw(self, plot):
+		self.left.draw(plot)
+		self.right.draw(plot)
+
+
+
+class Operator:
+	"""This class represents operations beeween data."""
+	
+	def __init__(self, operation):
+		self.operation = operation
+	
+	def eval(self, plot, data, elem):
+		"""This method evaluates the operation."""
+		return self.operation(plot, data, elem)
+		
+	def __add__(self, operator):
+		"""This method adds two operations."""
+		return Operator(
+			lambda plot, data, elem:
+				self.eval(plot, data, elem) +
+				operator.eval(plot, data, elem))
+	
+	def __sub__(self, operator):
+		"""This method substracts two operations."""
+		return Operator(
+			lambda plot, data, elem:
+				self.eval(plot, data, elem) -
+				operator.eval(plot, data, elem))
+		
+	def __mul__(self, operator):
+		"""This method multiplies two operations."""
+		return Operator(
+			lambda plot, data, elem:
+				self.eval(plot, data, elem) *
+				operator.eval(plot, data, elem))
+	
+	def __truediv__(self, operator):
+		"""This method divides two operations."""
+		return Operator(
+			lambda plot, data, elem:
+				self.eval(plot, data, elem) /
+				operator.eval(plot, data, elem))
+
+
+
+def Cons(value):
+	"""This functions returns a constant operator."""
+	return Operator(lambda plot, data, elem: value)
